b1e1b6fefe72ada3271741a12092f6263ff5cbeb
JeJune Lee <81547780+LeeJejune@users.noreply.github.com>
2025-08-06 17:19:39 +0900

    initail commit:)

---
+++ src/main/kotlin/com/alpha/archive/ArchiveApplication.kt
@@ -0,0 +1,11 @@
+package com.alpha.archive
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArchiveApplication
+
+fun main(args: Array<String>) {
+	runApplication<ArchiveApplication>(*args)
+}
---
+++ src/test/kotlin/com/alpha/archive/ArchiveApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alpha.archive
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArchiveApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
