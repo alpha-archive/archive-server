da6ab285d6d2f773557757f47159a31089ca2c1e
JeJune Lee <81547780+LeeJejune@users.noreply.github.com>
2025-09-09 12:34:47 +0900

    feature: user event entity add

---
+++ archive-domain/src/main/kotlin/com/alpha/archive/domain/event/embeddable/ActivityInfo.kt
@@ -0,0 +1,26 @@
+package com.alpha.archive.domain.event.embeddable
+
+import com.alpha.archive.domain.event.EventCategory
+import jakarta.persistence.Column
+import jakarta.persistence.Embeddable
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+
+@Embeddable
+data class ActivityInfo(
+    @Column(name = "custom_title", length = 300)
+    val customTitle: String? = null,
+
+    @Column(name = "custom_category", length = 50)
+    @Enumerated(EnumType.STRING)
+    val customCategory: EventCategory,
+
+    @Column(name = "custom_location", length = 200)
+    val customLocation: String? = null,
+
+    @Column(name = "rating", nullable = true)
+    val rating: Int? = null, // 1-5점 평점
+
+    @Column(name = "memo", columnDefinition = "text")
+    val memo: String? = null
+)
